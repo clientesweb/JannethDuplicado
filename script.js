be34512f1933ac2277e421052cef39a183e844ae
clientesweb <alexisifran32@gmail.com>
2025-02-03 07:59:11 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app")
   app.innerHTML = `
+        ${TopBanner()}
         ${Header()}
         <main>
             ${Hero()}
