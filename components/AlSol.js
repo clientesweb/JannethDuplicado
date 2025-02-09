f57134fb81b429c65545be64c42f771a28630b73
clientesweb <alexisifran32@gmail.com>
2025-02-09 14:32:17 -0300

    Actualizar AlSol.js

--- components/AlSol.js
+++ components/AlSol.js
@@ -23,7 +23,7 @@ function AlSol() {
     agent: {
       name: "Janneth Aguirre",
       phone: "+593 98 716 7782",
-      email: "janneth@jannethaguirre.com",
+      email: "info@jannethaguirrebienesraices.com",
     },
     status: "Pre Venta",
     type: "Casas",
